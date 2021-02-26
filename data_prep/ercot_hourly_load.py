b91db768ec2cf94b4fb46310786545d3629f811e
tailaiw <29800495+tailaiw@users.noreply.github.com>
2021-02-26 12:57:47 -0600

    added loder of hourly load data

---
+++ data_prep/ercot_hourly_load.py
@@ -0,0 +1,93 @@
+import json
+import os
+import zipfile
+from typing import List
+
+import numpy as np
+import pandas as pd
+import requests
+
+
+def parse_ercot_hourly_load(url_json: str, years: List[int]) -> pd.DataFrame:
+    """Parse ERCOR hourly load data
+
+    Parameters
+    ----------
+    url_json : str
+        Path to a local JSON file containing the URL of raw data load
+
+    years : list of integer
+        Years of which load data is parsed
+
+    Returns
+    -------
+    Pandas.DataFrame
+        Hourly load data as a Pandas DataFrame
+
+    """
+    with open(url_json, "r") as fr:
+        urls = json.load(fr)
+
+    if not os.path.exists("./tmp_data/"):
+        os.mkdir("./tmp_data/")
+
+    values = np.zeros((0, 9))
+    index = pd.DatetimeIndex([])
+    for year in years:
+        print(year)
+        url = urls[str(year)]  # type:str
+        r = requests.get(url)
+        local_path = os.path.join("./tmp_data/", url[url.rfind("/") + 1 :])
+        with open(local_path, "wb") as fwb:
+            fwb.write(r.content)
+
+        if url.endswith(".zip"):
+            excel_name = f"{year}.xlsx"
+            zipfile.ZipFile(local_path).extractall("./tmp_data/")
+            os.rename(
+                os.path.join("./tmp_data/", zipfile.ZipFile(local_path).namelist()[0]),
+                os.path.join("./tmp_data/", excel_name),
+            )
+            os.remove(local_path)
+        elif url.endswith(".xls"):
+            excel_name = f"{year}.xls"
+            os.rename(local_path, os.path.join("./tmp_data/", excel_name))
+        else:
+            raise RuntimeError("The file is not zip or xls.")
+
+        df = pd.read_excel(os.path.join("./tmp_data/", excel_name))
+        df = df.set_index(df.columns[0])
+        # deal with time stamp of 24:00:00
+        if df.index.dtype == "O":
+            df.index = pd.DatetimeIndex(
+                [s[:11] + str(int(s[11:13]) - 1) + s[13:] for s in df.index]
+            ) + pd.Timedelta("1H")
+        df.index = df.index.round(freq="S")
+        index = index.append(df.index)
+        values = np.vstack([values, df.values])
+
+    hourly_load = pd.DataFrame(
+        values,
+        index=index,
+        columns=[
+            "Coast",
+            "East",
+            "Far West",
+            "North",
+            "North Central",
+            "South",
+            "South Central",
+            "West",
+            "ERCOT",
+        ],
+    )
+    return hourly_load
+
+
+if __name__ == "__main__":
+    df = parse_ercot_hourly_load(
+        url_json="./ercot_hourly_load_urls.json",
+        years=list(range(2010, 2022)),
+    )
+
+    df.to_csv("./tmp_data/all.csv")
